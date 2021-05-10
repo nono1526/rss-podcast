fc12e59585937a2012716c5fd8b6b84cab43a3c9
Nono <nono1102137211@gmail.com>
2021-05-10 14:25:33 +0800

    feat: add PCover.vue

--- src/components/PCover.vue
+++ src/components/PCover.vue
@@ -1,5 +1,20 @@
 <template>
-  <div class="" />
+  <div class="flex p-5 bg-blue-400 text-white">
+    <img
+      class="max-w-lg w-60 border"
+      :alt="imageAlt"
+      :src="imageUrl"
+    >
+    <div class="flex-1 ml-5">
+      <div>{{ author }}</div>
+      <div class="text-2xl font-medium">
+        {{ title }}
+      </div>
+      <p class="whitespace-pre-wrap mt-3 max-w">
+        {{ description }}
+      </p>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -10,6 +25,18 @@ export default {
       default: ''
     },
     description: {
+      type: String,
+      default: ''
+    },
+    author: {
+      type: String,
+      default: ''
+    },
+    imageUrl: {
+      type: String,
+      default: ''
+    },
+    imageAlt: {
       type: String,
       default: ''
     }
