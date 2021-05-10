446efc42dcf91e512254d3ea8087d32b25ee26c8
Nono <nono1102137211@gmail.com>
2021-05-10 09:35:12 +0800

    feat: move data fetching to Home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,43 @@
 <template>
-  123123
+  <div class="">
+    <div />
+  </div>
 </template>
 
-<script setup>
+<script>
+import { onMounted, reactive, toRefs } from 'vue'
+import { fetchChannel } from '@/api/request.js'
+export default {
+  setup () {
+    const states = reactive({
+      title: '1',
+      image: {},
+      item: [],
+      description: ''
+    })
+    // This starter template is using Vue 3 experimental <script setup> SFCs
+    // Check out https://github.com/vuejs/rfcs/blob/script-setup-2/active-rfcs/0000-script-setup.md
+    const init = async () => {
+      const {
+        title,
+        image,
+        item,
+        description
+      } = await fetchChannel()
+      states.title = title
+      states.image = image
+      states.item = item
+      states.description = description
+    }
+    onMounted(() => {
+      init()
+    })
 
+    return {
+      ...toRefs(states)
+    }
+  }
+}
 </script>
 
 <style>
