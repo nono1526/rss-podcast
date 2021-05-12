318a1f9e8f42d4ed472fb92cd07d238dfe352367
Nono <nono1102137211@gmail.com>
2021-05-12 17:47:07 +0800

    feat: add PBtn

---
+++ src/components/PBtn.vue
@@ -0,0 +1,35 @@
+<template>
+  <button
+    class="focus:outline-none"
+    :class="getClasses()"
+  >
+    <slot />
+  </button>
+</template>
+
+<script>
+export default {
+  props: {
+    icon: {
+      type: Boolean,
+      default: false
+    }
+  },
+  setup (props) {
+    const getClasses = () => {
+      let classes = ''
+      if (props.icon) {
+        classes += 'rounded-full p-1 bg-white border transform hover:scale-110 transition transition-transform'
+      }
+      return classes.split(' ')
+    }
+    return {
+      getClasses
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
