664dc6f6ec9222eebd81b205e9050f05d2f15817
Nono <nono1102137211@gmail.com>
2021-05-12 17:46:09 +0800

    feat: update PPlayer

--- src/components/PPlayer.vue
+++ src/components/PPlayer.vue
@@ -1,6 +1,10 @@
 <template>
   <div
-    class="fixed bottom-0 w-full bg-blue-50 border-t h-20 flex"
+    class="bottom-0 w-full bg-blue-50 border-t h-20 flex"
+    :class="{
+      hidden: !visible,
+      fixed: fixed
+    }"
   >
     <img
       :src="cover"
@@ -15,37 +19,49 @@
     </div>
     <div class="flex-1 flex flex-col justify-center">
       <div
-        class="w-7/12 mx-auto flex items-center"
+        class="w-7/12 mx-auto"
       >
-        <span class="mr-3 text-gray-500 w-14 text-right text-sm">{{ playedTime }}</span>
-        <PSlider
-          :value="currentTime"
-          :max="duration"
-          @input="$emit('update:currentTime', $event)"
-          @dragstart="dragstart"
-          @dragend="dragend"
-        />
-        <span class="ml-3 w-14 text-gray-500 text-sm">{{ restTime }}</span>
+        <div class="text-center">
+          <PBtn icon>
+            <PlayIcon
+              :show-pause="isPlay"
+              @click="toggleAudioPlayState"
+            />
+          </PBtn>
+        </div>
+        <div class="w-full flex items-center">
+          <span class="mr-3 text-gray-500 w-14 text-right text-sm">{{ playedTime }}</span>
+          <PSlider
+            :value="currentTime"
+            :max="duration"
+            @input="$emit('update:currentTime', $event)"
+            @dragstart="dragstart"
+            @dragend="dragend"
+          />
+          <span class="ml-3 w-14 text-gray-500 text-sm">{{ restTime }}</span>
+        </div>
       </div>
     </div>
-
-    <button
-      class="border p-5"
-      @click="toggleAudio"
-    >
-      Pause
-    </button>
   </div>
 </template>
 
 <script>
-import { onMounted, watch, toRefs, reactive, computed } from 'vue'
+import { onMounted, watch, toRefs, reactive, computed, onUnmounted } from 'vue'
 import PSlider from '@src/components/PSlider.vue'
+import PlayIcon from '@src/components/PlayIcon.vue'
+import PBtn from '@src/components/PBtn.vue'
+
 export default {
   components: {
-    PSlider
+    PSlider,
+    PlayIcon,
+    PBtn
   },
   props: {
+    audioElement: {
+      type: Audio,
+      default: null
+    },
     url: {
       type: String,
       default: ''
@@ -73,27 +89,52 @@ export default {
     currentTime: {
       type: Number,
       default: 0
+    },
+    fixed: {
+      type: Boolean,
+      default: false
     }
   },
-  emits: ['update:isPlay', 'update:url', 'update:currentTime'],
+  emits: ['update:isPlay', 'update:url', 'update:currentTime', 'update:visible'],
   setup (props, { emit }) {
-    const audio = new Audio()
+    let audio
     let isDragging = false
     const states = reactive({
       played: 0,
       loaded: 0,
-      duration: 0
-    })
-    audio.addEventListener('loadedmetadata', e => {
-      states.duration = audio.duration
+      duration: 0,
+      isLoading: false
     })
 
-    audio.addEventListener('timeupdate', e => {
+    const onLoadmetadata = () => {
+      states.duration = audio.duration
+    }
+
+    const emitVisible = visible => {
+      emit('update:visible', visible)
+    }
+
+    const showPlayer = () => emitVisible(true)
+
+    const onTimeUpdate = e => {
       states.played = audio.currentTime / audio.duration
       states.loaded = audio.buffered.end(0) / audio.duration
+      states.isLoading = false
       if (isDragging) return
       emit('update:currentTime', audio.currentTime)
-    })
+    }
+
+    const bindAudioEvents = () => {
+      audio.addEventListener('loadedmetadata', onLoadmetadata)
+      audio.addEventListener('loadeddata', showPlayer)
+      audio.addEventListener('timeupdate', onTimeUpdate)
+    }
+
+    const unBindAudioEvents = () => {
+      audio.removeEventListener('loadedmetadata', onLoadmetadata)
+      audio.removeEventListener('loadeddata', showPlayer)
+      audio.removeEventListener('timeupdate', onTimeUpdate)
+    }
 
     const setAudioCurrentTime = currentTime => {
       audio.currentTime = currentTime
@@ -102,6 +143,7 @@ export default {
     const dragend = e => {
       if (!isDragging) return
       setAudioCurrentTime(props.currentTime)
+      states.isLoading = true
       isDragging = false
     }
 
@@ -138,6 +180,8 @@ export default {
     watch(() => props.url, url => {
       audio.src = url
       audio.currentTime = 0
+      states.isLoading = true
+      showPlayer()
       audio.play()
       emit('update:url', url)
       emit('update:isPlay', true)
@@ -145,16 +189,10 @@ export default {
 
     watch(() => props.isPlay, isPlay => {
       isPlay ? audio.play() : audio.pause()
-      emit('update:isPlay', isPlay)
     })
 
-    const toggleAudio = () => {
+    const toggleAudioPlayState = () => {
       const isPlay = props.isPlay
-      if (isPlay) {
-        audio.pause()
-      } else {
-        audio.play()
-      }
       emit('update:isPlay', !isPlay)
     }
 
@@ -163,10 +201,16 @@ export default {
     }
 
     onMounted(() => {
-      console.log(audio)
+      console.log(props.audioElement)
+      audio = props.audioElement || new Audio()
+      bindAudioEvents()
+    })
+
+    onUnmounted(() => {
+      unBindAudioEvents()
     })
     return {
-      toggleAudio,
+      toggleAudioPlayState,
       dragend,
       playedTime,
       restTime,
